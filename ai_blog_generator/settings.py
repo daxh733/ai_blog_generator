be0e8307bf53a196304a97ecb3ac26646a56df57
Daksh Tyagi <tyagidaksh360@gmail.com>
2025-11-17 21:47:33 +0530

    small change

--- ai_blog_generator/settings.py
+++ ai_blog_generator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-^nul+ps8s2q5!*%9@z1#jcxxp6qb2$refm&*wf)ay&nlaov+_n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
